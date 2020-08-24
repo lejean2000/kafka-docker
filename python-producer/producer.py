eda68cd58c2555318be744a178b344e494e6e2ec
Debian <admin@ip-10-0-0-70.eu-central-1.compute.internal>
2020-08-24 11:09:59 +0000

    modify producer to be configurable

--- python-producer/producer.py
+++ python-producer/producer.py
@@ -1,22 +1,35 @@
 """Test Kafka Producer
 """
 import os
+from datetime import datetime, timezone
 import time
 import json
 from random import randint
 from pykafka import KafkaClient
 from pykafka.common import CompressionType
 
+def produce_random(thekey: str, thespec: dict):
+    if thespec.get(thekey).get("type") == "enum":
+        vals = thespec.get(thekey).get("enum")
+        r = randint(0, len(vals) - 1)
+        return vals[r]
+
+    return ""
+
 if __name__ == '__main__':
     kafka_broker = os.environ['KAFKA_BROKER']
     topic = os.getenv('KAFKA_TOPIC', 'test_kafka_topic')
     use_rdkafka = os.getenv('USE_RDKAFKA') is not None
     use_snappy = CompressionType.SNAPPY if os.getenv('USE_SNAPPY') is not None else 0
+    print_every = int(os.getenv('PRINT_EVERY', '1'))
+
+    spec: dict = json.loads(os.environ['SPEC'])
 
     client = KafkaClient(hosts=kafka_broker)
     topic = client.topics[topic]
+
     if use_rdkafka:
-        print ("Using RDKafka")
+        print("Using RDKafka")
 
     producer = topic.get_producer(
         sync=True,
@@ -24,11 +37,32 @@ if __name__ == '__main__':
         use_rdkafka=use_rdkafka,
         compression=use_snappy
     )
+
     i = 0
+
+    machine = spec.pop("machine")
+
+    for key in spec:
+        spec[key]["time"] = datetime.now(timezone.utc)
+
     while True:
-        key = 'k' + str(randint(1, 1000))
-        msg = {'ts': round(1000*time.time()), 'id':key, 'value': randint(1, 1000)}
-        i = i + 1
-        producer.produce(json.dumps(msg).encode("utf-8"))
-        if i%1000 == 1:
-            print(i, "messages", json.dumps(msg))
+        now = datetime.now(timezone.utc)
+
+        for key in spec:
+            before = spec.get(key).get('time', now)
+            freq = spec.get(key).get("freq_seconds", 0)
+
+            # only produce message if frequency has passed
+            if (now-before).total_seconds() <= freq:
+                continue
+
+            spec[key]["time"] = now
+            generated = produce_random(key, spec)
+
+            msg = {"m":machine, "key":key, "value":generated, "ts":now.isoformat()}
+            i = i + 1
+            producer.produce(json.dumps(msg).encode("utf-8"))
+            if i%print_every == 0:
+                print(i, "messages", json.dumps(msg))
+
+        time.sleep(1)
